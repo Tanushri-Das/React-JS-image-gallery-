f1d917b66a4997e3486c0ccd43d89d998fc833ac
tanu <dastanushri402@gmail.com>
2023-11-01 19:12:29 +0600

    update hr border

--- src/Components/ImageGallery/ImageGallery.jsx
+++ src/Components/ImageGallery/ImageGallery.jsx
@@ -86,11 +86,11 @@ const ImageGallery = () => {
           "Gallery"
         )}
       </h3>
-      <hr />
+      <hr className="border-2 border-gray-100"/>
       {isLoading ? ( // Conditional rendering based on loading state
         <Spinner />
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 my-6">
           {images.map((image, index) => (
             <div
               key={image.id}
